7a7f1d266bb55ba760a2a114f2e3164b755b62ab
karolokienica <karolokienica@gmail.com>
2020-10-24 15:36:23 +0200

    added onclick handler

--- src/App.js
+++ src/App.js
@@ -3,16 +3,47 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 },
+    ],
+    otherState: 'some other value',
+  };
+
+  switchNameHandler = () => {
+    // console.log('Was clicked');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximiliam';
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className='App'>
         <h1>Hi, I am a React App, hi</h1>
         <p> This is really working</p>
-        <Person name='Max' age='29' />
-        <Person name='Manu' age='29'>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           My Hobbies: Racing
         </Person>
-        <Person name='Stephanie' age='26' />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     // return React.createElement(
