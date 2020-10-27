10f0bf5b3c21fbda32f972fe2acb45f023c0b3fc
karolokienica <karolokienica@gmail.com>
2020-10-27 14:03:50 +0100

    new syntax for passing reference

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
       <div className='App'>
         <h1>Hi, I am a React App, hi</h1>
         <p> This is really working</p>
-        <button onClick={this.switchNameHandler.bind(this, 'Maxilimilian')}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
