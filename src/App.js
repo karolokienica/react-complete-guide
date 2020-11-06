6c8d5592543b89a2417174275347df441afea055
karolokienica <karolokienica@gmail.com>
2020-11-06 16:15:33 +0100

    Changing to to CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import styled from 'styled-components';
-
-const StyledButton = styled.button`
-      background-color: ${props => props.alt ? 'red' : 'green'};
-      color: white;
-      font: inherit;
-      border: 1px solid blue;
-      padding: 8px;
-      cursor: pointer;
-
-      &:hover {
-        background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-        color: black;
-      }
-`;
 
 class App extends Component {
   state = {
@@ -115,8 +100,8 @@ persons[personIndex] = person;
       <div className='App'>
         <h1>Hi, I am a React App, hi</h1>
         <p className={classes.join(' ')}> This is really working</p>
-        <StyledButton alt={this.state.showPersons}
-        onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+        <button 
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
       
